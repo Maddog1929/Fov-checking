2f9d4409812efca36fa764aaf5ec02786974af17
Maddog1929 <cav4321@gmail.com>
2016-09-29 20:40:08 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <algorithm>
 #include <math.h>
 #include "Vector.h"
-
-inline float GetFov(Vector3 angle, Vector3 src, Vector3 dst)//pass your current viewangle, current 3d eye position, and 3d entity position (the cube)  <<this is what you call after confirming LOS. Everything else is called by this
+ //this is what you call after confirming LOS. Everything else is called by this
+inline float GetFov(Vector3 angle, Vector3 src, Vector3 dst)//pass your current viewangle, current 3d eye position, and 3d entity position (the cube) 
 {
 	Vector3 ang, aim;
 	ang = CalcAngle(src, dst);
@@ -16,9 +16,8 @@ inline float GetFov(Vector3 angle, Vector3 src, Vector3 dst)//pass your current 
 	return RAD2DEG(acos(u_dot_v / (pow(mag, 2))));  // will return an exact float for fov. 
 }
 
-/*if (this->GetFov(currentView, pLocal->GetEyePosition(), target) <= 80.0f) {
-					cmd->viewangles = oldViewangle;
-				}Heres an example for checking if a projectile is in our 80 fov
+/*if (this->GetFov(currentView, pLocal->GetEyePosition(), target) <= 80.0f) 
+	Heres an example for checking if a entity is in our 80 fov
 				*/
 
 inline Vector3 CalcAngle(Vector3 PlayerPos, Vector3 EnemyPos)//returns an angle in a vector mesureing difference between you and entity
@@ -35,7 +34,7 @@ inline Vector3 CalcAngle(Vector3 PlayerPos, Vector3 EnemyPos)//returns an angle 
 	return AimAngles;
 }
 
-inline void MakeVector(Vector3 angle, Vector3& vector) //idk what this does *make a vector i quess*, but it uses passing by refrence so your gona have to figure that out in c# which dosent do that i think.
+inline void MakeVector(Vector3 angle, Vector3& vector) //idk what this does *make a vector i guess*, but it uses passing by refrence so your gona have to use the REF command.
 {
 	float pitch = float(angle[0] * PI / 180);
 	float yaw = float(angle[1] * PI / 180);
